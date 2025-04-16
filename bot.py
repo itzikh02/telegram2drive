9e8cdf0c981df9eaad75516a246f1d398dc571d4
Itzik Hanan <itzikh02@gmail.com>
2025-04-16 14:10:26 +0300

    added slash after bot

--- bot.py
+++ bot.py
@@ -117,7 +117,7 @@ def main():
     app = Application.builder() \
         .token(BOT_TOKEN) \
         .base_url("http://localhost:8081/bot") \
-        .base_file_url("http://localhost:8081/file/bot") \
+        .base_file_url("http://localhost:8081/file/bot/") \
         .local_mode(True) \
         .build()
 
