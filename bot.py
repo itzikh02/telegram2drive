5467fe95eceffa21e51b31848de6d9390aebe029
Itzik Hanan <itzikh02@gmail.com>
2025-04-16 14:30:45 +0300

    app fix

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ def main():
     app = Application.builder() \
         .token(BOT_TOKEN) \
         .base_url("http://localhost:8081/bot") \
-        .base_file_url("http://localhost:8081/file/bot/") \
+        .base_file_url("http://localhost:8081/file") \
         .local_mode(True) \
         .build()
 
