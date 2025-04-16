f702e7ed5c22e89371088c9dbcdb55fc2c81791a
Itzik Hanan <itzikh02@gmail.com>
2025-04-16 22:57:58 +0300

    creds fix

--- drive_uploader.py
+++ drive_uploader.py
@@ -25,7 +25,7 @@ def get_drive_service():
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
