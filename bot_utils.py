211b2c15d6988e0812c476753821e35d92855646
Itzik Hanan <itzikh02@gmail.com>
2025-05-18 23:34:06 +0300

    fix bot_utils

--- bot_utils.py
+++ bot_utils.py
@@ -1,19 +1,13 @@
-import os
-from dotenv import load_dotenv
-from telegram import Bot
-
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-bot = Bot(token=BOT_TOKEN)  # מופע עצמאי
+from bot_application import app
 
 async def send_message(chat_id: str | int, text: str):
     """
-    שליחת הודעה דרך הבוט בצורה עצמאית.
-    
-    :param chat_id: מזהה הצ'אט או המשתמש.
-    :param text: תוכן ההודעה לשליחה.
+    Sends a message to a specified chat.
+    :param chat_id: The ID of the chat to send the message to.
+    :param text: The message text to send.
     """
+
     try:
-        await bot.send_message(chat_id=chat_id, text=text)
+        await app.bot.send_message(chat_id=chat_id, text=text)
     except Exception as e:
         print(f"[ERROR] Failed to send message: {e}")
