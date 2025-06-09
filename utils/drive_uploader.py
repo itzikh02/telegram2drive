9c19dbcf2a8f5856d0ccbf0ea841fec968c0c105
Itzik Hanan <itzikh02@gmail.com>
2025-06-09 22:11:48 +0300

    fix auth to drive

--- utils/drive_uploader.py
+++ utils/drive_uploader.py
@@ -6,23 +6,20 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 from utils.bot_utils import send_message
+from utils.auth_utils import require_auth
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
+@require_auth
 def get_drive_service():
     creds = None
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
 
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
-        else:
-            raise Exception("🚫 No valid token found. Run /auth to authenticate.")
+    else:
+        raise Exception("🚫 No valid token found. Run /auth to authenticate.")
 
     return build('drive', 'v3', credentials=creds)
 
